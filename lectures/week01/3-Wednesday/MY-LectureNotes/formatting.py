19d566096971586ab29fd51e61921dce56b2a38d
peyton.s <peytonshelley@rocketmail.com>
2022-07-06 14:30:40 -0500

    python notes

--- lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
+++ lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
@@ -1,15 +1,29 @@
 
 # Create a variable called first_name and set the value to your first name
 
+first_name = "peyton"
+
 # create a variable called last_name and set the value to your last name
+
+last_name = "shelley"
 
 # print your first name console
 
+print(first_name)
+
 # print your last name to the console
+
+print(last_name)
 
 # Create a new varaiable called my_name and set the value to the concatenation of your first name and your last name 
 
+my_name = f"{first_name} {last_name}=
+
 # print "My name is ???" out to the console.  Substitue the ??? with the my_name variable.
+
+print(f"My name is" {my_name}")
+
+
 
 # create a variable called company_name and set the value to "DigitalCrafts"
 
@@ -29,6 +43,11 @@
 # Email: harry.potter@DigitalCrafts.com
 # Username: harry_potter
 
+
+
+
+
+type
 
 
 

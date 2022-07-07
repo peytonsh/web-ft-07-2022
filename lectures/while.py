57804d00f8697c0053cbc79b2326705e96c77e36
peyton.s <peytonshelley@rocketmail.com>
2022-07-07 11:20:07 -0500

    notes

---
+++ lectures/while.py
@@ -0,0 +1,38 @@
+# initial_value = 0
+# while initial_value < 10: 
+#     initial_value += 1
+#     print(initial_value)
+    
+    
+    
+    #2
+# obj = 11
+# while obj >= 1:
+#     obj -=1
+#     print(obj)
+        
+        
+        #3
+# question = input ("enter a word  ")
+
+# while question.lower () != "stop":
+#     question = input ("enter a word  ")
+
+
+#4
+
+# username = 'DkillaK'
+# password = 'password123!'
+
+# login_attempt_username = "".lower()
+# login_attempt_password = "".lower()
+# attempts = 0
+
+# while (login_attempt_username != username or login_attempt_password != password) and attempts < 5: 
+#     login_attempt_username = input('enter a username')
+#     login_attempt_password = input('enter a password')
+#     attempts += 1
+#     if login_attempt_username == username and login_attempt_password == password: 
+#         print('welcome!')
+#     elif attempts == 5:
+#         print('too many attempts. Restart program and try again.')

355c4b6ba9e1cae6ab7277612e60edae57e239f8
peyton.s <peytonshelley@rocketmail.com>
2022-07-08 09:38:38 -0500

    list.practice.py

---
+++ lectures/list.Practice.py
@@ -0,0 +1,43 @@
+# # #1
+
+# # planets = ["earth", "jupitor", "neptune","mars", "saturn", "mercury", "uranus", "venus"]
+
+
+
+# # # #2
+# # num_planets = len(planets)
+# # print(num_planets)
+ 
+ 
+# # #  #3
+# # count = 0
+ 
+# # while count < num_planets:
+# #       print[planets[count].lower()]
+# #       count +=1
+
+# # #4
+# # planets.append("pluto")
+
+
+# HoustonCities = ["Katy", "Memorial City", "Sugar Land",
+#                 "The Heights", "River Oaks", "Pasadena"]
+
+# ClearLakeCities = ["League City", "Kemah", "Seabrook", "Webster", "El Lago"]
+
+# houston = HoustonCities + ClearLakeCities
+
+# num_cities = len(houston)
+# print(num_cities)
+
+# more_cities = ["Dallas", "Austin"]
+# houston.append(more_cities)
+
+# index = 0 
+# while 
+
+›
+
+
+
+

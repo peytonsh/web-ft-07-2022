0c8c9051a38b2f9397b1ee19ba6ed7523f04fcb4
peyton.s <peytonshelley@rocketmail.com>
2022-07-26 10:23:51 -0500

    l

---
+++ lectures/week04/1-Monday/MY-LectureNotes/drills.js
@@ -0,0 +1,82 @@
+// for (let counter = 0; counter <= 10; counter++){
+//     console.log(counter);
+//   }
+
+
+//   for (let counter = 0; counter < 4; counter++){
+//     console.log(counter);
+//   }
+
+
+//   for (let counter = 10; counter >= 1; counter--){
+//     console.log(counter);
+//   }
+
+
+//  for (let name =1; name <= 10; name++){
+//     console.log("peyton")
+//  }
+
+//  for (let counter = 5; counter <= 10; counter ++){
+//     console.log(counter)
+//  }
+
+//  for (count = 1; count <= 10; count++)
+//     {
+//     console.log(count * 5)
+//  }
+
+//  let p = 4
+//  let sum = 1
+//  for (let x =1; x <= p; x++){
+//     sum = sum * x
+//  }
+//  console.log(sum)
+
+
+//  let sal = 58000
+
+
+//  for (let x = 1; x <= 5; x++){
+//     sal = sal * .02 + sal
+//     console.log(sal)
+//  }
+ 
+
+// let num2 = 8; 
+// let sum = 0
+
+// for (let x = 1; x <= num2; x++){
+//     sum = sum += x 
+// }
+// console.log(sum)
+
+
+// for ()
+
+// let n = 10;
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     if(i % 2 != 0){
+//         sum += i;
+//     }
+    
+// }
+
+// console.log(sum)
+
+// let num = 10000
+// num.toString().length
+// let numOfDigits = num.toString().length
+// console.log(numOfDigits)
+
+
+
+
+
+
+let num = 45623556567822 
+
+let numStr = num.toString() 
+
+console.log(`first ${numStr[0]} last ${numStr[numStr.length - 1]}`);
